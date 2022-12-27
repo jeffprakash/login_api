e6eea727a3e63b3122feb300e75cb921a73bde09
jeffprakash <jeffprakash@gmail.com>
2022-12-27 23:25:50 +0530

    inital

---
+++ restapi/urls.py
@@ -0,0 +1,32 @@
+"""restapi URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from api import views
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path('register/',views.register.as_view(),name='register'),
+    path('login/',obtain_auth_token,name="login"),
+    path('welcome/',views.welcome.as_view(),name="welcome"),
+    path('userDetails/<int:pk>/',views.userDetails.as_view(),name="userDetails"),
+    path('paginationApi',views.paginationApi.as_view(),name="paginationApi")
+    
+   
+    
+]
