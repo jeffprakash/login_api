45a39bcbebdb79e14f757998223971d4fc68cd25
jeffprakash <jeffprakash@gmail.com>
2022-12-28 00:18:17 +0530

    new2

--- restapi/settings.py
+++ restapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm9o@2cffb)!y^xz+1u-kdw1=7z39gy(yc7*%sawu9fpbsqc=8y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -123,7 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
